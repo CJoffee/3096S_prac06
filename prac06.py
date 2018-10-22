00f74fab8aa5ee387ce7a503162013d4a5c660c0
meetbhalia <meetharish_1@hotmail.com>
2018-10-22 15:07:31 +0000

    Turn detetection somewhat working, it seems ovely sensitive to jostling, and if you turn it too slow if fails to register as turning. Pending meet soldering the pot wires or something

--- prac06.py
+++ prac06.py
@@ -10,11 +10,16 @@ import Adafruit_MCP3008
 comboDir = [1,0,1]
 comboLog = [2,1,2]
 tic = 0
+delay = 150.0
+tolerance = 0.05
 
 # Log for 16 readings from potentiometer/Knob
-log = [None]*16	# Start empty
-dir = [None]*16
-n_log = 0;	# List iteration variable
+log = []	# Start empty
+dir = []
+v_before = 0	# Starting position of the pot, will be read before loop
+mode = 0	# 0 = secure, 1 = unsecure
+dir_prev = 2	# Previous direction of turning
+#n_log = 0;	# List iteration variable
 
 # Set GPIO to BCM numbering
 GPIO.setmode(GPIO.BCM)
@@ -24,34 +29,39 @@ sec_mode = 5
 unsec_mode = 6
 
 def secure(channel):
-	potVal = [0]*16
-	potVal.append(ConvertVolts(mcp.read_adc(7), 4))
-	potVal.pop(0)
-	bef = [0]*16
+	global mode
+	mode = 0
+	#global mcp
+	#potVal = []
+	#potVal.append(ConvertVolts(mcp.read_adc(7), 4))
+	#potVal.pop(0)
+	#bef = [0]*16
 
-	if ((comboLog == log) and (comboDir == dir)):
-		GPIO.output(unlock, GPIO.HIGH)
-		time.sleep(2)
-		GPIO.output(unlock, GPIO.LOW)
-	else:
-		GPIO.output(lock, GPIO.HIGH)
-		time.sleep(2)
-		GPIO.output(lock, GPIO.LOW)
+	#if ((comboLog == log) and (comboDir == dir)):
+	#	GPIO.output(unlock, GPIO.HIGH)
+	#	time.sleep(2)
+	#	GPIO.output(unlock, GPIO.LOW)
+	#else:
+	#	GPIO.output(lock, GPIO.HIGH)
+	#	time.sleep(2)
+	#	GPIO.output(lock, GPIO.LOW)
 
 def unsecure(channel):
-	sort(combolog)
-	sort(log)
-	for i in range(len(combolog)):
+	global mode
+	mode = 1
+	#sort(combolog)
+	#sort(log)
+	#for i in range(len(combolog)):
 		# Check element-wise equality
-		if combolog[i] != log[i]:
-			# If not equal, blink LOCK, then break loop
-			GPIO.output(lock, GPIO.HIGH)
-			time.sleep(2)
-			GPIO.output(lock, GPIO.LOW)
-			break
-	GPIO.output(unlock, GPIO.HIGH)
-	time.sleep(2)
-	GPIO.output(unlock, GPIO.LOW)
+	#	if combolog[i] != log[i]:
+	#		# If not equal, blink LOCK, then break loop
+	#		GPIO.output(lock, GPIO.HIGH)
+	#		time.sleep(2)
+	#		GPIO.output(lock, GPIO.LOW)
+	#		break
+	#GPIO.output(unlock, GPIO.HIGH)
+	#time.sleep(2)
+	#GPIO.output(unlock, GPIO.LOW)
 
 
 # GPIO setup for buttons, set event detectors
@@ -93,7 +103,8 @@ def ConvertVolts(data,places):
 
 # Reset timer
 def reset():
-    timer = time.time()
+	global timer
+    	timer = time.time()
 
 # Python sort, select sort
 def sort(list):
@@ -112,21 +123,59 @@ def sort(list):
 # Check direction pot is turning
 def checkDir(before, after):
 	#tolerance for negligable changes in volts
-	tolerance = 0.5    
+	global tolerance
 	if (before - after > tolerance):
-		return 0 	#Left turn
+		return 0 	#CW turn
 	elif (after - before > tolerance):
-		return 1 	#Right turn
+		return 1 	#CCW turn
 	else:
 		return 2 	#No turn
 
 	
 def main():
+	v_before = ConvertVolts(mcp.read_adc(7), 4)
+	global delay
+	dir_curr = 2
+	twos = 0
+	global dir_prev
+	global tic
 	try:
 		while True:
-			testlog = [0 2 1]
-			sort(testlog)
-			//print(testlog)
+			#testlog = [0, 2, 1]
+			global log
+			global dir
+			v_read = ConvertVolts(mcp.read_adc(7), 4)
+			dir_curr = checkDir(v_before, v_read)
+			#if (dir_curr==2):
+				#twos = twos+1
+			if(dir_curr != 2 and dir_prev!=dir_curr and twos < 2000/delay):
+				# start timer again
+				tic = 0
+				print("Current direction: ", dir_curr)
+
+				#print("Previous direction: ", dir_prev)
+
+				print("Timer started!")
+			if(dir_curr == 2 and dir_prev!=dir_curr):
+				# stop timer
+				log.append(tic*delay)
+				dir.append(dir_prev)
+
+				print("Timing (ms): ", tic*delay)
+				print("Direction: ", dir_prev)
+			#print(dir_curr)
+
+			if(mode == 0):
+					#secure mode
+					pass
+			if(mode == 1):
+					#unsecure mode
+					pass
+			v_before = v_read
+			dir_prev = dir_curr
+			tic = tic+1
+#			print("TICK!")
+			time.sleep(float(delay/1000))
 
 	except KeyboardInterrupt:
 		GPIO.cleanup()
